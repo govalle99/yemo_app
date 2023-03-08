cbf7d329cef862e21e9e009f745e4ee2c13390f6
unknown <govalle99@gmail.com>
2023-03-08 15:14:12 -0600

    tercera

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('yemo app'),
+        ),
+        body: Text('body'),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'email',
+              icon: Icon(Icons.email),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(Icons.add),
+            ),
+            BottomNavigationBarItem(
+              label: 'Notifications',
+              icon: Icon(Icons.notifications),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
